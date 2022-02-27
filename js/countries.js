0909c135d0bc1a29065ef3a6c13fc5235889476e
sabbirpboss <sabbir.ah799@gmail.com>
2022-02-27 11:44:30 +0600

    call dynamic api, country details

--- js/countries.js
+++ js/countries.js
@@ -13,13 +13,33 @@ const displayCountries = (countries) => {
     countries.forEach(country => {
         const div = document.createElement('div');
         div.classList.add('country');
-        const h3 = document.createElement('h3');
-        h3.innerText = country.name;
-        div.appendChild(h3);
-        const p = document.createElement('p');
-        p.innerText = country.capital;
-        div.appendChild(p);
+        div.innerHTML = `
+        <h3>${country.name}</h3>
+        <p>${country.capital}</p>
+        <button onclick="loadCountryByName('${country.name}')">Details</button>
+        `
+        // const h3 = document.createElement('h3');
+        // h3.innerText = country.name;
+        // div.appendChild(h3);
+        // const p = document.createElement('p');
+        // p.innerText = country.capital;
+        // div.appendChild(p);
         countriesDiv.appendChild(div);
     });
 }
 
+const loadCountryByName = name => {
+    const url = `https://restcountries.com/v2/name/${name}`;
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayCountryDetail(data[0]));
+}
+
+const displayCountryDetail = country => {
+const countryDiv = document.getElementById('country-detail');
+countryDiv.innerHTML = `
+    <h5>Country name: ${country.name}</h5>
+    <p>Population: ${country.population}</p>
+    <img width="200px" src = "${country.flag}">
+`
+}
