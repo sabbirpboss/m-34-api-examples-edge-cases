458303bfc4be2da4f8d7125d4b39d623964bda8a
sabbirpboss <sabbir.ah799@gmail.com>
2022-03-01 11:07:42 +0600

    fetch the url and convert it to json

--- meals/js/mealdb.js
+++ meals/js/mealdb.js
@@ -1,2 +1,16 @@
-const array = { hobbies: ["dancing", "singing", "acting"] };
-console.log(JSON.stringify(array));
+const searchFood = () => {
+    const searchField = document.getElementById('search-field');
+    //get input value
+    const searchText = searchField.value;
+    console.log(searchText);
+    //do search field empty
+    searchField.value = '';
+    //set the url with https and make it *dynamic
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`
+
+    console.log(url);
+    //fetch the url and convert to json
+    fetch(url)
+    .then(res => res.json())
+    .then(data => console.log(data.meals));
+}
