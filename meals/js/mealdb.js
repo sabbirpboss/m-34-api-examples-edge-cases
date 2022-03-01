6a44ac35a6a4c9213f64880bee9268365c327e97
sabbirpboss <sabbir.ah799@gmail.com>
2022-03-01 11:44:40 +0600

    successfully displayed search detail

--- meals/js/mealdb.js
+++ meals/js/mealdb.js
@@ -28,7 +28,7 @@ const displaySearchResult = meals => {
             <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
             <div class="card-body">
                 <h5 class="card-title">${meal.strMeal}</h5>
-                 <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                 <p class="card-text">${meal.strInstructions.slice(0, 100)}</p>
             </div>
         </div>
         `;
