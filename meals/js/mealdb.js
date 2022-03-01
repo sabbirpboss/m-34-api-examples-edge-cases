2dbecd3e7648ee315750d836d64406754b2a3912
sabbirpboss <sabbir.ah799@gmail.com>
2022-03-01 11:37:53 +0600

    successfully displayed search picture and title

--- meals/js/mealdb.js
+++ meals/js/mealdb.js
@@ -25,9 +25,9 @@ const displaySearchResult = meals => {
         div.classList.add('col');
         div.innerHTML = `
         <div class="card">
-            <img src="..." class="card-img-top" alt="...">
+            <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
             <div class="card-body">
-                <h5 class="card-title">Card title</h5>
+                <h5 class="card-title">${meal.strMeal}</h5>
                  <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
             </div>
         </div>
